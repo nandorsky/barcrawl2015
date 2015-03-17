5f75e508ce54c9faf9138997fc21b1c4bbea3181
Nate Andorsky <nandorsky@gmail.com>
2015-03-17 00:47:17 -0400

    installing facbeook authentication

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -234,6 +234,9 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
+  # Facebook Authentication
+  config.omniauth :facebook, "682419001869396", "b615d757a13cddc086ed3fef0046b0be"
+
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150317040828) do
+ActiveRecord::Schema.define(version: 20150317044047) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "comment"
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 20150317040828) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
