1af4be66fac0d1bc83f0d993279b88473b8a6a1f
Nate Andorsky <nandorsky@gmail.com>
2015-04-08 14:06:29 -0400

    added my account page and also login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
   resources :votes
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks",  :registrations => "registrations" }
+  resources :users
   resources :comments
-  root to: 'pages#home'
+  
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
